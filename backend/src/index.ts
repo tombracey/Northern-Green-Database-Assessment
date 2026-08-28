9616a431c565da3c92e23d9995ed0d954444d923
Tom Bracey <tombracey22@gmail.com>
2026-08-28 23:29:01 +0100

    Added .listen() to stop the backend crashing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { pool } from "./db";
 
 const app = express();
+const port = 3001;
 
 app.get("/locations", async (_req: express.Request, res: express.Response) => {
   try {
@@ -11,4 +12,8 @@ app.get("/locations", async (_req: express.Request, res: express.Response) => {
     console.error(err);
     res.status(500).json({ error: "Database error" });
   }
+});
+
+app.listen(port, () => {
+  console.log(`Backend running on http://localhost:${port}`);
 });
